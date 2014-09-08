90a707af96efd0d9dcd89ca35b8b2f2c169d0248
cgm616 <cgm616@me.com>
2014-09-07 20:15:57 -0500

    recipes added

--- java/com/cgm616/common/Main/MainRegistry.java
+++ java/com/cgm616/common/Main/MainRegistry.java
@@ -1,5 +1,10 @@
 package com.cgm616.common.Main;
 
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
 import com.cgm616.common.lib.RefStrings;
 
 import cpw.mods.fml.common.Mod;
@@ -8,6 +13,7 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = RefStrings.MODID, name = RefStrings.NAME, version = RefStrings.VERSION)
 public class MainRegistry {
@@ -17,20 +23,22 @@ public class MainRegistry {
 	
 	@EventHandler
 	public static void PreLoad(FMLPreInitializationEvent PreEvent) {
-		
 		proxy.registerRenderInfo();
-		
 	}
 	
 	@EventHandler
 	public static void Load(FMLInitializationEvent Event) {
-		
+		AddRecipes();
 	}
 	
 	@EventHandler
 	public static void PostLoad(FMLPostInitializationEvent PostEvent) {
 		
 	}
-
+	
+	private static void AddRecipes() {
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.string, 3), Blocks.wool);
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.web, 1), "XYX", " X ", "X X", 'X', Items.string, 'Y', Items.slime_ball);
+	}
 
 }
