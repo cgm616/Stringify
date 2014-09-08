3d846d84f908f2e7dbfc88976a6aea28f860ccef
cgm616 <cgm616@me.com>
2014-09-07 19:40:39 -0500

    starting mod

---
+++ java/com/cgm616/common/Main/MainRegistry.java
@@ -0,0 +1,28 @@
+package com.cgm616.common.Main;
+
+import com.cgm616.common.lib.RefStrings;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = RefStrings.MODID, name = RefStrings.NAME, version = RefStrings.VERSION)
+public class MainRegistry {
+	
+	@EventHandler
+	public static void PreLoad(FMLPreInitializationEvent PreEvent) {
+		
+	}
+	@EventHandler
+	public static void Load(FMLInitializationEvent Event) {
+		
+	}
+	@EventHandler
+	public static void PostLoad(FMLPostInitializationEvent PostEvent) {
+		
+	}
+
+
+}
---
+++ java/com/cgm616/common/lib/RefStrings.java
@@ -0,0 +1,9 @@
+package com.cgm616.common.lib;
+
+public class RefStrings {
+	
+	public static final String MODID = "stringify";
+	public static final String NAME = "Stringify";
+	public static final String VERSION = "0.0.1";
+
+}
