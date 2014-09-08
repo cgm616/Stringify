c0a4842e7365f1bf892180be2c38838af3ad0472
cgm616 <cgm616@me.com>
2014-09-07 20:47:55 -0500

    added wool fuel and adjusted balance

--- java/com/cgm616/common/handlers/FuelHandler.java
+++ java/com/cgm616/common/handlers/FuelHandler.java
@@ -1,6 +1,8 @@
 package com.cgm616.common.handlers;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.IFuelHandler;
 
@@ -10,6 +12,9 @@ public class FuelHandler implements IFuelHandler{
 	public int getBurnTime(ItemStack fuel) {
 		if(fuel.getItem() == Items.string) {
 			return 30;
+		}
+		if(fuel.getItem() == Item.getItemFromBlock(Blocks.wool)) {
+			return 140;
 		}
 		return 0;
 	}
